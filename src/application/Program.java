3f3e2649c178b6821e34bba5a9dae75f9f779c3a
pedro-batista <peddrobatista2022@gmail.com>
2024-02-12 18:31:43 -0300

    Segundo commit...

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 public class Program {
 	public static void main(String[] args) {
-		List<Integer> intList = new ArrayList<Integer>();
-		intList.add(10);
-		intList.add(5);
-		List<? extends Number> list = intList;
-		Number x = list.get(0);
-		list.add(20); // erro de compilacao
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+		List<? super Number> myNums = myObjs;
+		myNums.add(10);
+		myNums.add(3.14);
+		Number x = myNums.get(0); // erro de compilacao
 	}
 }
