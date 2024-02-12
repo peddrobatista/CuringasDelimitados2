e9e0ece0d97d8efc5f80eacdd5ab94d5c15d57a6
pedro-batista <peddrobatista2022@gmail.com>
2024-02-12 18:39:26 -0300

    Terceiro commit ...

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,30 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
 	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		List<Object> myObjs = new ArrayList<Object>();
-		myObjs.add("Maria");
-		myObjs.add("Alex");
-		List<? super Number> myNums = myObjs;
-		myNums.add(10);
-		myNums.add(3.14);
-		Number x = myNums.get(0); // erro de compilacao
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
+	
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for(Number number : source) {
+		destiny.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+		System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 }
